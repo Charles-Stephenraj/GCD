bca4314dc1617a92a21e59a622f2eadc2f9b10c4
Charles Stephenraj J <123827682+Charles-Stephenraj@users.noreply.github.com>
2023-07-26 22:58:43 +0530

    gcd.java

--- gcd.java
+++ gcd.java
@@ -4,12 +4,14 @@ public class gcd
 	public static void main(String args[])
 	{
 		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the no of numbers to find gcd");
 		int n=sc.nextInt();
 		int a[]=new int[n];
 		//int d[]=new int[100];
 		ArrayList<Integer> d=new ArrayList<Integer>();
 		ArrayList<Integer> e=new ArrayList<Integer>();
 		int c=0,k=0;
+		System.out.println("Enter the numbers");
 		for(int i=0;i<n;i++)
 		{
 			a[i]=sc.nextInt();
